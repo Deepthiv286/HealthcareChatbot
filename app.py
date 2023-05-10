fcb5e49523667cdbbc0223935abae73ca7fba93b
Deepthi V <deepthi.v@mantralabsglobal.com>
2023-05-10 20:00:07 +0530

    streamlit

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 #from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
+import streamlit as st
+
+header=st.container()
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 #CORS(app) #comment this on deployment
 api = Api(app)
+
+with header:
+    st.header(" USA Covid-19 Data Analysis")
+    st.text("This is the data analysis of USA Covid-19 of 2020 from March to December")
 
 @app.route("/", defaults={'path':''})
 def serve(path):
