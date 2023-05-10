cd656b71925dd94b9976ad997de0bc9418f66ae8
Deepthi V <deepthi.v@mantralabsglobal.com>
2023-05-10 20:26:10 +0530

    sidebar

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
-#from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+# from flask import Flask, send_from_directory
+# from flask_restful import Api, Resource, reqparse
+# #from flask_cors import CORS #comment this on deployment
+# from api.HelloApiHandler import HelloApiHandler
 import streamlit as st
 
-header=st.container()
+# header=st.container()
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-#CORS(app) #comment this on deployment
-api = Api(app)
+# app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+# #CORS(app) #comment this on deployment
+# api = Api(app)
 
-with header:
-    st.header(" USA Covid-19 Data Analysis")
-    st.text("This is the data analysis of USA Covid-19 of 2020 from March to December")
+st.sidebar.title("NLP Bot")
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+# @app.route("/", defaults={'path':''})
+# def serve(path):
+#     return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+# api.add_resource(HelloApiHandler, '/flask/hello')
