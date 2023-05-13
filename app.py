911dced41069e6914911f03dcb475423792b9368
Deepthi V <deepthi.v@mantralabsglobal.com>
2023-05-13 15:31:31 +0530

    changes

--- app.py
+++ app.py
@@ -41,9 +41,6 @@ if (st.sidebar.button('Submit')):
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
 def query(payload):
 	return "Hello"+ str(random.randint(0, 50))
 
@@ -51,30 +48,25 @@ def get_text():
     input_text = st.text_input("You: ", key="input")
     return input_text 
 
-st.session_state.generated.append("Please enter symptoms separated by comma(,)")
+st.session_state.generated.append({"text":"Please enter symptoms separated by comma(,)", "is_user": False})
 
 user_input = get_text()
 
 if user_input:
     output = query({
         "inputs": {
-            "past_user_inputs": st.session_state.past,
             "generated_responses": st.session_state.generated,
             "text": user_input,
         },"parameters": {"repetition_penalty": 1.33},
     })
 
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+    st.session_state.generated.append({"text": user_input, "is_user": True})
+    st.session_state.generated.append({"text": output, "is_user": False})
 
 if st.session_state['generated']:
 
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        if st.session_state["generated"][i]:
-            message(st.session_state["generated"][i], key=str(i))
-
-        if len(st.session_state['past']) > 0:
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        message(st.session_state["generated"][i].text, is_user=st.session_state["generated"][i].is_user, key=str(i))
 
 # message_history.append(
 #     {"text": "Please enter symptoms separated by comma(,)", "is_user": False})
