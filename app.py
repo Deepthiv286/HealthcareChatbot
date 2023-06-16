a1a6dbebeae6dbe785a959edc833b5980640665a
Deepthi V <deepthiv286@gmail.com>
2023-06-16 18:29:39 +0530

    device index 0

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def get_voice_response():
     try:
 
         # use the microphone as source for input.
-        with sr.Microphone(device_index=2) as source2:
+        with sr.Microphone(device_index=0) as source2:
 
             # wait for a second to let the recognizer
             # adjust the energy threshold based on
